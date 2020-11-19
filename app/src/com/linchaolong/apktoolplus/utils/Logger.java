75f73e99831e87c7789242ba67e06b3810c543a1
greatfox-lin <462915202@qq.com>
2020-11-19 19:55:42 +0800

    Logger.error

--- app/src/com/linchaolong/apktoolplus/utils/Logger.java
+++ app/src/com/linchaolong/apktoolplus/utils/Logger.java
@@ -10,9 +10,17 @@ public class Logger {
     public static void print(String msg){
         System.out.println(msg);
     }
+
     public static void print(String format, String... msg){
         System.out.printf((format) + "%n", msg);
     }
 
+    public static void error(String msg){
+        System.err.println(msg);
+    }
+
+    public static void error(String format, String... msg){
+        System.err.printf((format) + "%n", msg);
+    }
 
 }
